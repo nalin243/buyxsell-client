85995c726fcfa71cc165f1ed7400b9afc03804ef
Subhayan0022 <msubhayan529@gmail.com>
2023-02-14 20:28:47 +0530

    Changing file locations

---
+++ src/components/BuyerPage.js
@@ -0,0 +1,13 @@
+// import Footer from "./Footer"
+// import Header from "./Header"
+
+function BuyerPage (props){
+    return(
+        <div class="flex flex-col overflow-y-scroll background-page min-h-screen w-screen flex">
+			<Header />
+        </div>
+    )
+    
+}
+
+export default BuyerPage
