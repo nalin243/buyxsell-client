bbfff9370fc3b78ec15fba2e9e1f6675153e2b09
Subhayan0022 <msubhayan529@gmail.com>
2023-02-15 16:25:49 +0530

    Created BuyerPage Component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,14 +1,14 @@
 import BuyerCard from "./Homepage/BuyerCard"
 import SellerCard from "./Homepage/SellerCard"
-import Footer from "./Homepage/Footer"
-import Header from "./Homepage/Header"
+import Footer from "./Footer"
+import Header from "./Header"
 
 function HomePage(props){
 
 	return (
 
 		<div class="flex md:justify-end flex-col overflow-y-scroll background-page min-h-screen w-screen flex">
-			<Header/>
+			<Header homepage={true} />
 			<div class="overflow-hidden  flex justify-center h-full">
 				<BuyerCard/>
 				<SellerCard/>
