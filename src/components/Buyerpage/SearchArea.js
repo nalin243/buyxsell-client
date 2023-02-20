6ec335db7a6242a581985f09ccc448812b56d7b1
nalin243 <devpriyanalin@gmail.com>
2023-02-20 18:19:57 +0530

    Ui tweaks

--- src/components/Buyerpage/SearchArea.js
+++ src/components/Buyerpage/SearchArea.js
@@ -7,11 +7,11 @@ import Searchbar from "./Searchbar"
 function SearchArea(props){
 	return (
 
-		<div id="search-area" class="flex flex-col fixed backdrop-blur inset-0  h-screen w-screen">
+		<div id="search-area" class="flex hidden flex-col fixed backdrop-blur inset-0  h-screen w-screen">
 			<div class="flex w-full  backdrop-blur">
 					<img onClick={()=>{
-						document.getElementById("search-area").classList.add("hidden")
-						document.getElementById("search-bar").classList.remove("hidden")
+						document.getElementById("search-area").classList.toggle("hidden")
+						document.getElementById("search-bar").classList.toggle("hidden")
 					}} class="hover:cursor-pointer ml-7 mt-7 h-10 w-10" src={backicon}></img>
 					<input type="search" placeholder="Search for products..." class="placeholder-gray-800 border-b-4 rounded-sm border-black bg-transparent w-full mt-5 ml-4"/>
 					<img src={searchicon} class="h-10 w-10 hover:cursor-pointer -ml-10 mt-5 mr-12"/>
--- src/components/Buyerpage/Searchbar.js
+++ src/components/Buyerpage/Searchbar.js
@@ -4,8 +4,8 @@ function SearchBar(props) {
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
             <div class="relative -mt-60">
                 <input onClick={()=>{
-                    document.getElementById("search-area").classList.remove("hidden")
-                    document.getElementById("search-bar").classList.add("hidden")
+                    document.getElementById("search-area").classList.toggle("hidden")
+                    document.getElementById("search-bar").classList.toggle("hidden")
                 }}type="search" class="flex bg-white 200 w-full p-4 pl-4 rounded-lg placeholder-gray-400" placeholder="Search Products..." required/>
         </div>
     </div>
