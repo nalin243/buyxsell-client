19feac7cb3c462d37e9fbd6ff8024c81a9777d55
nalin243 <devpriyanalin@gmail.com>
2023-02-22 17:40:48 +0530

    Fixed accidental render during authcheck and adding loading feature to buyerpage and sellerpage

--- src/components/BuyerPage.js
+++ src/components/BuyerPage.js
@@ -5,39 +5,46 @@ import Footer from "./Footer"
 import Header from "./Header"
 
 import {useNavigate} from "react-router-dom"
-import {useEffect} from "react"
+import {useEffect,useState} from "react"
 
 import axios from "axios"
 
 function BuyerPage (props){
 
     const navigate = useNavigate()
-
-    // useEffect(()=>{
-    //     if(!props.loginStatus){
-    //             axios.get(process.env.REACT_APP_SERVER_URL+"authcheck",{
-    //             headers: {
-    //                 Authorization: localStorage.getItem("userTokenBuyer")
-    //             }
-    //         })
-    //         .then((response)=>{
-    //             if(!response.data.success)
-    //                 navigate("/home")
-    //         })
-    //     }
-    // })
-
-        return(
-            <div class="flex flex-col background-page min-h-screen w-screen flex">
-    			<Header page={"buyerpage"} />
-                <div class="flex flex-col justify-center h-screen w-screen">
-                <h1 class="flex m-auto -mb-22 justify-center buyer-page-pname ml-auto mt-auto">BUYxSELL</h1>
-                    <SearchBar/>
+    const [loading,updateLoading] = useState(true)
+ 
+        if(!props.loginStatus){
+                axios.get(process.env.REACT_APP_SERVER_URL+"authcheck",{
+                headers: {
+                    Authorization: localStorage.getItem("userTokenBuyer")
+                }
+            })
+            .then((response)=>{
+                if(!response.data.success)
+                    navigate("/home")
+                else
+                    updateLoading(false)
+            })
+            .catch((err)=>{
+                if(err.response.data==="Unauthorized")
+                    navigate("/home")
+            })
+        }
+ 
+        if(!loading){
+            return(
+                <div class="flex flex-col background-page min-h-screen w-screen flex">
+        			<Header page={"buyerpage"} />
+                    <div class="flex flex-col justify-center h-screen w-screen">
+                    <h1 class="flex m-auto -mb-22 justify-center buyer-page-pname ml-auto mt-auto">BUYxSELL</h1>
+                        <SearchBar/>
+                    </div>
+                    <SearchArea/>
+                    <Footer/>
                 </div>
-                <SearchArea/>
-                <Footer/>
-            </div>
-        )
+            )
+        }
     
     
 }
--- src/components/SellerPage.js
+++ src/components/SellerPage.js
@@ -5,34 +5,40 @@ import Header from "./Header"
 import SellerProfile from "./Sellerpage/SellerProfile"
 
 import {useNavigate} from "react-router-dom"
-import {useEffect} from "react"
+import {useEffect,useState} from "react"
 
 function SellerPage(props) {
 
     const navigate = useNavigate()
+    const [loading,updateLoading] = useState(true)
 
-    //  useEffect(()=>{
-    //     if(!props.loginStatus){
-    //             axios.get(process.env.REACT_APP_SERVER_URL+"authcheck",{
-    //             headers: {
-    //                 Authorization: localStorage.getItem("userTokenSeller")
-    //             }
-    //         })
-    //         .then((response)=>{
-    //             if(!response.data.success)
-    //                 navigate("/home")
-    //         })
-    //     }
-    // })
-        return ( 
-            <div class="flex flex-col background-page min-h-screen w-screen flex">
-                <Header page={"sellerpage"}/>
-                <div class="flex flex-col justify-center h-screen w-screen">
-                    <SellerProfile/>
+        if(!props.loginStatus){
+                axios.get(process.env.REACT_APP_SERVER_URL+"authcheck",{
+                headers: {
+                    Authorization: localStorage.getItem("userTokenSeller")
+                }
+            })
+            .then((response)=>{
+                if(!response.data.success)
+                    navigate("/home")
+            })
+            .catch((err)=>{
+                if(err.response.data==="Unauthorized")
+                    navigate("/home")
+            })
+        }
+
+        if(!loading){
+            return ( 
+                <div class="flex flex-col background-page min-h-screen w-screen flex">
+                    <Header page={"sellerpage"}/>
+                    <div class="flex flex-col justify-center h-screen w-screen">
+                        <SellerProfile/>
+                    </div>
+                    <Footer/>
                 </div>
-                <Footer/>
-            </div>
-         )
+             )
+        }
 }
 
 export default SellerPage;
