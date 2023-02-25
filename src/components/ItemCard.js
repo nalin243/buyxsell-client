b879707211861caaf270e71e3c1ce295be89d74c
nalin243 <devpriyanalin@gmail.com>
2023-02-25 21:07:29 +0530

    Fixed ui in deals area of sellerpage

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -60,24 +60,26 @@ function ItemCard(props){
 		return (
 
 			<div class="item-card shrink-0 m-8 mr-14 md:ml-14 xl:ml-16 flex flex-col rounded-xl md:h-5/6 md:w-5/12 xl:w-2/12 xl:h-5/6">
-				<div class="px-5 bg-black py-7 my-3 rounded-full  m-auto md:h-5/6 xl:h-4/6 md:w-7/12 xl:w-5/12">
+
+				<div class="px-5 bg-black py-7 my-3 m-auto md:h-5/6 xl:h-5/6 md:w-9/12 xl:w-10/12">
 				</div>
 
 				<div class="text-center mb-4">
-					<h1 class="product-name text-white text-2xl m-auto">{props.name}</h1>
+					<h1 class="product-name font-black text-2xl m-auto">{props.name}</h1>
 				</div>
 
 				<div class="flex flex-col w-full h-full">
-					<div class="flex">
-						<h1 class="product-name text-2xl ml-2">Price: {props.price}</h1>
-					</div>
 
-					<div class="overflow-y-scroll border-2 border-red-400 h-1/2 w-full flex">
-						<h1 class="product-name text-black text-2xl ml-2 mt-2">Description: {props.description}</h1>
+					<div class="overflow-y-scroll h-1/2 w-full flex">
+						<h1 class="product-name text-black font-medium text-2xl ml-2 mt-2">{props.description}</h1>
 					</div>
 
 					<div class="flex">
 						<h1 class="product-name text-black text-2xl ml-2 mt-2">Sold to: {props.soldto}</h1>
+					</div>
+
+					<div class="flex text-center w-full mt-2">
+						<h1 class="product-name m-auto text-2xl ">Rs {props.price}</h1>
 					</div>
 				</div>
 			</div>
--- src/components/Sellerpage/DealsArea.js
+++ src/components/Sellerpage/DealsArea.js
@@ -23,35 +23,39 @@ function DealsArea(props){
                 return sum+item.price
             },0))
             updateDeals((response.data.result).map((item)=>{
-                return <ItemCard comploc={"seller"} name={item.name} description={item.description} soldto={item.soldto} price={item.price} />
+                if(item.sold)
+                    return <ItemCard comploc={"seller"} name={item.name} description={item.description} soldto={item.soldto} price={item.price} />
             })) 
         })
     })
     return (
         <div class="flex flex-col h-full w-full">
-            <div class="flex flex-row  mt-10 m-auto h-5/6 w-11/12">
-                <div class="flex flex-col m-5 stage-area h-5/6 w-6/12">
-                    <div class="flex flex-1  h-full w-full">
-                        <h1 class="product-name m-auto text-5xl">Total Items Sold</h1>
+            <div class="flex flex-row justify-center mt-10 m-auto h-4/6 w-11/12">
+                <div class="flex flex-col m-5 stage-area h-4/6 w-5/12">
+                    <div class="flex flex-1 h-full w-full">
+                        <h1 class="product-name m-auto text-5xl pt-2">Total Items Sold</h1>
                     </div>
                     <div class="flex flex-2  h-full w-full">
                         <h1 class="product-name m-auto text-5xl">{totalItemsSold}</h1>
                     </div>
                 </div>
-                <div class="flex flex-col m-5 stage-area h-5/6 w-6/12">
+                <div class="flex flex-col m-5 stage-area h-4/6 w-5/12">
                     <div class="flex flex-1 h-full w-full">
-                        <h1 class="product-name m-auto text-5xl">Total Sales</h1>
+                        <h1 class="product-name m-auto text-5xl pt-2">Total Sales</h1>
                     </div>
                     <div class="flex flex-2  h-full w-full">
                         <h1 class="product-name m-auto text-5xl">{totalSales}</h1>
                     </div>
                 </div>
             </div>
-            <div class="flex ml-20 h-40 w-80">
-                <h1 class="product-name text-6xl">Previous Sales</h1>
-            </div>
-            <div class="flex overflow-x-scroll h-inherit min-w-full">
-                {deals}
+
+            <div class="flex flex-col h-4/6 w-full">
+                <div class="flex ml-20 w-full">
+                    <h1 class="product-name text-6xl mb-2">Previous Sales</h1>
+                </div>
+                <div class="flex overflow-x-scroll h-screen min-w-full">
+                    {deals}
+                </div>
             </div>
 
         </div>
