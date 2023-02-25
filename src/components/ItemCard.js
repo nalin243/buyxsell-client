eec44bb0b852fbb3b240b8bbdabcced9c8e607df
nalin243 <devpriyanalin@gmail.com>
2023-02-25 09:59:17 +0530

    Updated itemcard ui

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,24 +1,34 @@
+const carticon = require('./../assets/shopping-cart-white.png')
+
 function ItemCard(props){
 
 	if (props.comploc==="buyer"){
 
 		return (
 
-			<div class="item-card m-8 mr-14 md:ml-14 xl:ml-16 flex flex-col rounded-xl md:h-3/6 md:w-4/12 xl:w-2/12 xl:h-3/6">
-				<div class="px-5 bg-black py-7 my-3 rounded-full  m-auto h-5/6 w-7/12">
+			<div class="item-card m-8 mr-14 md:ml-14 xl:ml-16 flex flex-col rounded-xl md:h-3/6 md:w-4/12 xl:w-3/12 xl:h-5/6">
+
+				<div class="px-5 bg-black py-7 my-3 mt-7 m-auto h-5/6 w-10/12">
 				</div>
 
 				<div class="text-center">
-					<h1 class="product-name text-white text-2xl m-auto">Item detail</h1>
+					<h1 class="product-name text-black text-2xl m-auto mb-7 font-extrabold">Test item 1</h1>
 				</div>
 
 				<div class=" w-full h-full">
-					<div>
-						<h1 class="product-name text-2xl ml-2">100,000</h1>
+					<div class="overflow-y-scroll">
+						<h1 class="product-name text-black text-2xl ml-2 mt-2 px-5 font-normal">Item description test. This is an extremely great item that you will want to buy as soon as possible</h1>
 					</div>
 
-					<div class="overflow-hidden">
-						<h1 class="product-name text-black text-2xl ml-2 mt-2">Bhai ekdam tagda product hai ekbar lekar toh dekho bhai</h1>
+					<div class="text-center">
+						<h1 class="product-name font-black text-2xl mt-6 -mb-6">Rs 100,000</h1>
+					</div>
+
+					<div class="flex justify-center mt-10">
+						<div class="flex hover:cursor-pointer justify-end bg-black -mt-2 py-3 rounded-md w-3/6 ">
+							<h1 class="mr-4 text-white">Add to cart</h1>
+							<img class="h-6 w-6 mr-8" src={carticon}/>
+						</div>
 					</div>
 				</div>
 			</div>
